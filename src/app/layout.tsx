d2c02058e3a5cb00b60fd2d670c38c01c4905b3b
박창조 <ckdwh1202@gmail.com>
2025-09-08 15:51:14 +0900

    feat: Pretentard 폰트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
 const notoSansKr = Noto_Sans_KR({
   variable: "--font-noto-sans-kr",
   subsets: ["latin"],
+});
+
+const Pretendard = localFont({
+  src: "../../public/fonts/PretendardVariable.woff2",
+  variable: "--font-pretendard",
 });
 
 export const metadata: Metadata = {
@@ -19,7 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={`${notoSansKr.variable} antialiased`}>{children}</body>
+      <body
+        className={` ${Pretendard.variable} ${notoSansKr.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
