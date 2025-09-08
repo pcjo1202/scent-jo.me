062e8fdacb4c3ce0f043d5bbb30e0a9d1b87ab62
박창조 <ckdwh1202@gmail.com>
2025-09-08 16:06:39 +0900

    feat: 간단 seo 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,51 @@ const Pretendard = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "냄새나는 개발자 박창조",
-  description: "안녕하세요. 프론트엔드 개발자 박창조입니다.",
+  title: "향기나는 개발자 박창조",
+  description:
+    "안녕하세요. 동료와 사용자에게 탁월한 경험을 전달하는 프론트엔드 개발자 박창조입니다.",
+  icons: {
+    icon: "/favicon.png",
+  },
+  openGraph: {
+    images: ["/favicon.png"],
+  },
+  metadataBase: new URL("https://scent-jo.me"),
+  alternates: {
+    canonical: "https://scent-jo.me",
+  },
+  twitter: {
+    card: "summary_large_image",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  manifest: "/manifest.json",
+  themeColor: "#000000",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  appleWebApp: {
+    title: "향기나는 개발자 박창조",
+    statusBarStyle: "black-translucent",
+  },
+  authors: [{ name: "박창조", url: "https://scent-jo.me" }],
+  creator: "박창조",
+  publisher: "박창조",
+  category: "technology",
+  keywords: [
+    "프론트엔드",
+    "개발자",
+    "박창조",
+    "향기나는 개발자",
+    "포트폴리오",
+    "신입",
+    "프론트엔드 개발자",
+  ],
+  applicationName: "향기나는 개발자 박창조",
 };
 
 export default function RootLayout({
