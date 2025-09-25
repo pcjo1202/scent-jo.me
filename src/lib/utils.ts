d81903b3fd8609b53b83f5779a23a5edfb6fb48a
박창조 <ckdwh1202@gmail.com>
2025-09-26 02:32:00 +0900

    feat: throttle 함수 구현

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,33 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+// 개선 (진짜 throttle)
+export function throttle(func: () => void, delay = 100) {
+  let lastExecuted = 0;
+  let timeout: NodeJS.Timeout | null = null;
+
+  return () => {
+    const now = Date.now();
+
+    if (now - lastExecuted >= delay) {
+      // 즉시 실행
+      lastExecuted = now;
+      func();
+    } else {
+      // 지연 실행으로 마지막 호출 보장
+      if (timeout) clearTimeout(timeout);
+      timeout = setTimeout(
+        () => {
+          lastExecuted = Date.now();
+          func();
+          timeout = null;
+        },
+        delay - (now - lastExecuted)
+      );
+    }
+  };
 }
