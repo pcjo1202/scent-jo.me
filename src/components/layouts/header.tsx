cb8e10575bb0bc822815fef695aa2118b0cd436f
박창조 <ckdwh1202@gmail.com>
2025-09-30 03:16:50 +0900

    feat: header 반응형 수정

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { cn, throttle } from "@/lib/utils";
+import { useScrollPosition } from "@/hooks/useScrollPosition";
+import { cn } from "@/lib/utils";
 import {
   CodeIcon,
   FolderIcon,
@@ -10,7 +11,6 @@ import {
   UserIcon,
 } from "lucide-react";
 import Link from "next/link";
-import { useCallback, useEffect, useRef, useState } from "react";
 import { InteractiveHoverButton } from "../ui/interactive-hover-button";
 
 const navigation = [
@@ -22,42 +22,9 @@ const navigation = [
 ];
 
 export default function Header() {
-  const [isScrolled, setIsScrolled] = useState<"up" | "down">("up");
-  const [isTop, setIsTop] = useState(true);
-  const lastScrollY = useRef(0); // 마지막 스크롤 위치
+  const { isScrolled, scrollY } = useScrollPosition();
 
-  const handleScroll = useCallback(
-    throttle(() => {
-      const currentScrollY = window.scrollY; // 현재 스크롤 위치
-      let isUpScroll = false;
-
-      if (currentScrollY < 5) {
-        setIsScrolled("up");
-        setIsTop(true);
-        return;
-      }
-
-      // 스크롤 방향이 위로 올라가는 경우
-      if (currentScrollY < lastScrollY.current) {
-        isUpScroll = true;
-      }
-
-      if (Math.abs(currentScrollY - lastScrollY.current) > 100) {
-        setIsScrolled(isUpScroll ? "up" : "down");
-        setIsTop(false);
-        lastScrollY.current = currentScrollY;
-      }
-    }, 100),
-    [setIsScrolled, setIsTop]
-  );
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll, { passive: true });
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  const isTop = scrollY < 50;
 
   return (
     <>
@@ -65,20 +32,22 @@ export default function Header() {
         className={cn(
           "flex fixed top-0 z-50 w-full justify-between items-center md:px-20 px-10 py-4",
           "transition-all duration-300 ease-out",
-          isScrolled === "up"
-            ? isTop
-              ? "bg-transparent"
-              : "translate-y-0 bg-gray-200/10 backdrop-blur-sm"
-            : "translate-y-[-100%] bg-transparent"
+          isScrolled && !isTop ? "" : ""
         )}
       >
         {/* logo */}
-        <div className="text-xl font-bold">
+        <div
+          className={cn(
+            "basis-1/3",
+            "text-xl font-bold transition-all duration-300 ease-out",
+            isTop ? "opacity-100" : "opacity-0"
+          )}
+        >
           <Link href="/">CHANG JO's Portfolio</Link>
         </div>
 
         {/* navigation - Desktop */}
-        <div className="flex items-center gap-8">
+        <div className="flex justify-center items-center gap-8 flex-1">
           <nav className="md:flex hidden">
             <ul className="flex gap-8">
               {navigation.map(({ label, href }) => {
@@ -93,15 +62,15 @@ export default function Header() {
               })}
             </ul>
           </nav>
-          <div className="hidden lg:flex items-center gap-4">
-            <button className="text-sm font-medium cursor-pointer hover:scale-110 transition-all duration-300 hover:bg-gray-200 rounded-xl p-1">
-              <MoonIcon strokeWidth={2} />
-              {/* <SunIcon strokeWidth={2} /> */}
-            </button>
-            <InteractiveHoverButton className="text-sm font-medium ">
-              연락하기
-            </InteractiveHoverButton>
-          </div>
+        </div>
+        <div className="hidden lg:flex items-center justify-end gap-4 basis-1/3">
+          <button className="text-sm font-medium cursor-pointer hover:scale-110 transition-all duration-300 hover:bg-gray-200 rounded-xl p-1">
+            <MoonIcon strokeWidth={2} />
+            {/* <SunIcon strokeWidth={2} /> */}
+          </button>
+          <InteractiveHoverButton className="text-sm font-medium ">
+            연락하기
+          </InteractiveHoverButton>
         </div>
       </header>
 
