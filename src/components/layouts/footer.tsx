c49bbd210f68ece3c8651c3e9b10e34c6c704259
박창조 <ckdwh1202@gmail.com>
2025-09-30 03:08:49 +0900

    feat: footer 구현

---
+++ src/components/layouts/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="text-center text-sm text-gray-500">
+      <p className="py-5">
+        Copyright © 2025 Park Chang Jo. All rights reserved.
+      </p>
+    </footer>
+  );
+}
