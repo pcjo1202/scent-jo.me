8c707a2dc32b050acc83d441febf34d28f9e0368
박창조 <ckdwh1202@gmail.com>
2025-10-08 03:41:33 +0900

    feat: hero section 수정

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -1,39 +1,36 @@
-import IconContainer from "@/components/home-section/icon-container";
-import Image from "next/image";
+import { ChevronsDownIcon } from "lucide-react";
 
 export default function HeroSection() {
   return (
-    <div id="home" className="h-dvh w-full">
-      <div className="flex flex-col items-center justify-between h-full gap-6">
+    <div id="home" className="h-dvh w-full snap-start">
+      <div className="flex flex-col items-center justify-center h-full pb-40 gap-6">
         {/* title */}
-        <div className="flex flex-col mt-40 items-center justify-center gap-6">
-          <h1 className="text-4xl font-bold flex justify-center items-center gap-2">
-            <span>프론트엔드 개발자</span>
-            <span className="text-blue-500">박창조</span>
-            <span>입니다. </span>
+        <div className="w-full flex flex-col justify-center gap-6">
+          {/* frontend developer */}
+          <h2 className="text-xl md:text-3xl font-bold flex flex-col justify-center  gap-2">
+            <span className="">Frontend Developer</span>
+          </h2>
+          <h1 className="text-5xl md:text-7xl font-bold flex flex-col justify-center  gap-2">
+            <span className="text-black">Park Chang Jo</span>
+            <span className="text-gray-400">I build Web for people</span>
           </h1>
-          <div className="text-lg text-gray-500 font-bold">
-            <p className="text-center">
-              사용자를 먼저 생각하는{" "}
-              <span className="text-black">역지사지</span> 마인드
-            </p>
-            <p className="text-center">
-              동료와 <span className="text-black">함께 성장</span>하는
-              개발자입니다.
+        </div>
+        {/* description */}
+        <div className="w-full flex flex-col justify-center gap-6">
+          <div className="text-base md:text-lg text-gray-500 font-bold">
+            <p>안녕하세요. 저는 냄새나는 프론트엔드 개발자 박창조입니다.</p>
+            <p>
+              <span>소프트웨어의 가치</span>가&nbsp;
+              <span className="text-blue-400">Code</span>와 &nbsp;
+              <span className="text-blue-400">User Experience</span>
+              {""}에 스며들도록 개발을 하고 있습니다.
             </p>
           </div>
         </div>
-        {/* link */}
-        <IconContainer />
-        {/* 사진? */}
-        <div className="flex flex-col items-center justify-center gap-6">
-          <Image
-            src="/assets/profile.png"
-            alt="Profile"
-            width={300}
-            height={300}
-          />
-        </div>
+      </div>
+      {/* scroll down */}
+      <div className="absolute bottom-10 left-1/2 -translate-x-1/2 animate-bounce">
+        <ChevronsDownIcon className="w-10 h-10 text-gray-400" />
       </div>
     </div>
   );
