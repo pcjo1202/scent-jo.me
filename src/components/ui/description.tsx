6d9eccd4c08f3123b4deb4f6fcafae90cbe6affc
박창조 <ckdwh1202@gmail.com>
2025-10-14 02:48:08 +0900

    feat: react bit UI component 추가

---
+++ src/components/ui/description.tsx
@@ -0,0 +1,13 @@
+export default function Description({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="w-full flex flex-col justify-center gap-6">
+      <div className="text-base md:text-lg text-gray-500 font-semibold">
+        {children}
+      </div>
+    </div>
+  );
+}
